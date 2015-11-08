a174a975c0e8d913a2dca3c4fa676a9cc93d2938
Tsuji Toshiyuki <toshibp@gmail.com>
2015-11-08 13:15:33 +0900

    add a plot about 1st Codons

--- js/jquery.drawbutton.js
+++ js/jquery.drawbutton.js
@@ -41,7 +41,19 @@ function drawPlots(){
   var num = makeNumbers(seq);
   num.unshift("AA");
   var nls = makeRegions(sosuireg,seq.length);
+  var positiveRegs = makePositiveChargeRichRegions(seq,charge,charge_residues,nls);
   nls.unshift("NLS");
+  positiveRegs.unshift("PositiveRegions");
+  
+  var firstcodons = getFirstCodonsSeq(mrna);
+  var adenine = calcMovingAverage(firstcodons,_a,8);
+  adenine.unshift("A");
+  var guanine = calcMovingAverage(firstcodons,_g,8);
+  guanine.unshift("G");
+  var thymine = calcMovingAverage(firstcodons,_t,8);
+  thymine.unshift("T");
+  var cytosine = calcMovingAverage(firstcodons,_c,8);
+  cytosine.unshift("C");
   // var sosui = makeRegions(sosuireg,seq.length);
   // sosui.unshift("NLS");
   // var signal = makeRegions(signalreg,seq.length);
@@ -93,9 +105,37 @@ function drawPlots(){
     },
 
     "data": [
-      num_rna,
+      num,
       at,
       gc
+     ]
+    };
+
+    var atgc_1st_data = {
+
+    "config": {
+      "type": "line",
+      "bg": "#fff",
+      "xScaleFont": "12px Helvetica",
+      "xScaleYOffset": 8,
+      "xScaleSkip":50,
+      "axisXLen":4,
+      "maxY": 1.0,
+      "minY": 0.0,
+      "paddingBottom": 0,
+      "width": graphwidth,
+      "height": graphheight,
+      "shadows":{"all":['#fff', 0, 0, 100]},
+      "colorSet": 
+            ["#DDA0DD","#0068b7","#3CB000","#ffa500"]
+    },
+
+    "data": [
+      num,
+      adenine,
+      thymine,
+      guanine,
+      cytosine      
      ]
     };
 
@@ -157,17 +197,21 @@ function drawPlots(){
       "type": "area",
       "shadows":{"all":['#fff', 0, 0, 100]},
       "colorSet": 
-        ["rgba(204,204,102,0.5","rgba(153,204,255,0.5)"]
+        ["rgba(153,204,255,0.5)","rgba(204,204,102,0.5"]
     },
 
     "data": [
       num,
+      positiveRegs,
       nls
 //      sosui,
 //      signal
     ]
   };
 
+    ccchart
+      .init('firstcodons', atgc_1st_data)
+      .after(sosuipred)
     ccchart
       .init('purine', purinedata)
       .after(sosuipred)
@@ -188,17 +232,6 @@ function markSeq(sequence,tmstr,sigstr){
   var sosui = makeRegions(tmstr,sequence.length);
   var s = ''
   for(var i=0;i<seq.length;i++){
-    // if(signal[i]==1){
-      // s += '<span class="signal">'+seq[i];
-      // for(i=i+1;i<seq.length;i++){
-      //   if(signal[i] == 1){
-      //     s += seq[i];
-      //   }else{
-      //     s += '</span>'+seq[i];
-      //     break;
-      //   }
-      // }
-    // }else if(sosui[i]==1){
     if(sosui[i]==1){
       s += '<span class="tm">'+seq[i];
       for(i=i+1;i<seq.length;i++){
@@ -217,7 +250,7 @@ function markSeq(sequence,tmstr,sigstr){
     s += "</span>";
   }
   return s;
-}
+};
 
 function markSeq4mRna(sequence,tmstr,sigstr){
   var seq = sequence.split('');
@@ -225,17 +258,6 @@ function markSeq4mRna(sequence,tmstr,sigstr){
   // var signal = makeRegions4mrna(sigstr,sequence.length);
   var s = ''
   for(var i=0;i<seq.length;i++){
-    // if(signal[i]==1){
-      // s += '<span class="signal">'+seq[i];
-      // for(i=i+1;i<seq.length;i++){
-      //   if(signal[i] == 1){
-      //     s += seq[i];
-      //   }else{
-      //     s += '</span>'+seq[i];
-      //     break;
-      //   }
-      // }
-    // }else if(sosui[i]==1){
     if(sosui[i]==1){
       s += '<span class="tm">'+seq[i];
       for(i=i+1;i<seq.length;i++){
@@ -254,7 +276,7 @@ function markSeq4mRna(sequence,tmstr,sigstr){
     s += "</span>";
   }
   return s;
-}
+};
 
 var kd = {'A': 1.8, 'R':-4.5, 'N':-3.5, 'D':-3.5, 'C': 2.5, 
          'Q':-3.5, 'E':-3.5, 'G':-0.4, 'H':-3.2, 'I': 4.5, 
@@ -263,6 +285,11 @@ var kd = {'A': 1.8, 'R':-4.5, 'N':-3.5, 'D':-3.5, 'C': 2.5,
 
 var charge = {'A': 0, 'R': 1.0, 'N': 0, 'D':-1.0, 'C': 0, 
          'Q': 0, 'E':-1.0, 'G': 0, 'H': 1.0, 'I': 0, 
+         'L': 0, 'K': 1.0, 'M': 0, 'F': 0, 'P': 0, 
+         'S': 0, 'T': 0, 'W': 0, 'Y': 0, 'V': 0 };
+
+var charge_residues = {'A': 0, 'R': 1.0, 'N': 0, 'D':1.0, 'C': 0, 
+         'Q': 0, 'E': 1.0, 'G': 0, 'H': 1.0, 'I': 0, 
          'L': 0, 'K': 1.0, 'M': 0, 'F': 0, 'P': 0, 
          'S': 0, 'T': 0, 'W': 0, 'Y': 0, 'V': 0 };
 
@@ -279,7 +306,10 @@ var negative = {'A': 0, 'R': 0, 'N': 0, 'D':-1.0, 'C': 0,
 var _purine = {'A':1.0,'G':1.0,'T':0.0,'C':0.0};
 var _at = {'A':1.0,'G':0.0,'T':1.0,'C':0.0};
 var _gc = {'A':0.0,'G':1.0,'T':0.0,'C':1.0};
-
+var _a = {'A':1.0,'G':0.0,'T':0.0,'C':0.0};
+var _t = {'A':0.0,'G':0.0,'T':1.0,'C':0.0};
+var _g = {'A':0.0,'G':1.0,'T':0.0,'C':0.0};
+var _c = {'A':0.0,'G':0.0,'T':0.0,'C':1.0};
 
 function calcMovingAverage(seq,values,width){
     var s = seq.split('');
@@ -301,6 +331,29 @@ function calcMovingAverage(seq,values,width){
     }
     return ma;
 };
+
+function getFirstCodonsSeq(seq){
+  var s = seq.split('');
+  firsts = [];
+  for(var i=0;i<s.length;i+=3){
+    firsts += s[i];
+  }
+  return firsts;
+};
+
+function makePositiveChargeRichRegions(seq,charge,charge_residues,nls){
+    var net = calcMovingAverage(seq,charge,8);
+    var charge = calcMovingAverage(seq,charge_residues,8);
+    positions = []
+    for (i=0;i<net.length;i++){
+      if(net[i] > 0 && charge[i] > 0.55 /*&& nls[i] == 0*/){
+        positions[i] = 1;
+      }else{
+        positions[i] = 0;
+      }
+    }
+    return positions;
+}
 
 function makeNumbers(seq){
   var num = [];
